766628c17038c3c3128bc625d09cd5c4eede1c2f
Bryan0xFF <bjmejia@correo.url.edu.gt>
2018-09-02 16:21:32 -0600

    Added new UI for basic display and init of display Adapter

--- Laboratorio0/app/src/main/java/com/example/bryanmeja/laboratorio0/MainActivity.java
+++ Laboratorio0/app/src/main/java/com/example/bryanmeja/laboratorio0/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bryanmeja.laboratorio0;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.ArrayMap;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.bryanmeja.laboratorio0.models.Cancion;
@@ -13,10 +16,38 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listaCanciones;
     Map<String,Cancion> canciones = new ArrayMap<>();
+    Button btnBuscar;
+    EditText editTextBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        StartArray();
+        btnBuscar = findViewById(R.id.btnBuscar);
+        editTextBuscar = findViewById(R.id.editTextCancion);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: buscar cancion por nombre y desplegarlo en nuevo array
+            }
+        });
+    }
+
+    private void StartArray(){
+
+        canciones.put("La buena y la mala",new Cancion("La buena y la mala",
+                "Saber","3:30","4"));
+        canciones.put("En Alabama", new Cancion("En Alabama","Les Fleurs du Mal",
+                "2:37","8"));
+        canciones.put("Origem",new Cancion("Origem","The Quantum Enigma",
+                "1:57","7"));
+        canciones.put("Crimson Bow and Arrow", new Cancion("Crimson Bow and Arrow",
+                "Epica vs Attack On Titan","5:42","10"));
+        canciones.put("Hola Mundo", new Cancion("Hola Mundo","Android","1:30",
+                "3"));
+
     }
 }
--- Laboratorio0/app/src/main/java/com/example/bryanmeja/laboratorio0/models/Cancion.java
+++ Laboratorio0/app/src/main/java/com/example/bryanmeja/laboratorio0/models/Cancion.java
@@ -3,14 +3,14 @@ package com.example.bryanmeja.laboratorio0.models;
 public class Cancion {
     public String cancion;
     public String album;
-    public double duracion;
+    public String duracion;
     public int rating;
 
     public Cancion(String cancion, String album, String duracion, String rating){
 
         this.cancion = cancion;
         this.album = album;
-        this.duracion = Double.parseDouble(duracion);
-        this.rating = Integer.parseInt(rating);
+        this.duracion = duracion;
+        this.rating = Integer.parseInt(rating); //rating es de 1-10
     }
 }
